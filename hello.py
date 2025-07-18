3f14e567e1dbc1d48f9f4d410549669016263a1b
Tininho Fita <tinofranciel@gmail.com>
2025-07-18 11:21:57 -0300

    atualizacao funcional

--- hello.py
+++ hello.py
@@ -1,47 +1,69 @@
 #!/usr/bin/env python3
+"""Hello world multi linguas.
 
-""" Hello world multi linguas.
+Dependendo da lingua configurada no ambiente, o programa o programa exibe a mensagem
+correspondente
 
-Dependendo da lingua configurada no ambiente, o programa irá imprimir "Hello World" na língua correspondente.
+como usar:
 
-como usar: 
+tenha a variável de ambiente LANG configurada exemplo:
 
-tenha a variável de ambiente LANG configurada para a língua desejada.
-
-Exemplo:    
     export LANG=pt_BR
 
-Execuão: 
+ou informe atraves do CLI argument `--lang`
+
+ou o usuário digitar.
+
+Execucão:
+
     python3 hello.py
     ou
     ./hello.py
-
-Versão: 0.0.1
 """
-__version__ = "0.0.1"
+__version__ = "0.1.3"
 __author__ = "Tininho Fita"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello World!"
+arguments: dict[str, str | int | None] = {"lang": None, "count": 1}
 
-if current_language == "pt_BR":
-    msg = "Olá Mundo!"
-elif current_language == "es_ES":
-    msg = "¡Hola Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour le monde!"
-elif current_language == "de_DE":
-    msg = "Hallo Welt!"
-elif current_language == "it_IT":
-    msg = "Ciao Mondo!"
-elif current_language == "ja_JP":
-    msg = "こんにちは世界！"
-elif current_language == "zh_CN":
-    msg = "你好，世界！"
-elif current_language == "ru_RU":
-    msg = "Привет, мир!"
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
 
-print(msg) 
+    if key not in arguments:
+        print(f"invalid option `{key}`")
+        sys.exit()
+
+    if key == "count":
+        try:
+            value = int(value)
+        except ValueError:
+            print(f"Valor inválido para count: {value}")
+            sys.exit()
+
+    arguments[key] = value
+
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repeticao
+    if "LANG" in os.environ:
+        current_language = os.environ["LANG"]
+    else:
+        current_language = input("Choose a language:")
+
+current_language = str(current_language)[:5]
+
+msg = {
+    "en_US": "Hello World!",
+    "pt_BR": "Olá Mundo!",
+    "it_IT": "Ciao Mondo!",
+    "es_ES": "¡Hola Mundo!",
+    "fr_FR": "Bonjour le monde!",
+}
+
+print(msg[current_language] * int(arguments["count"] or 1))
